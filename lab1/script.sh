3152f1ecc111ddd57898afecf568bd543d9da8f8
Vlada Mizerak <mizerak.vlada@gmail.com>
2022-12-02 13:51:06 +0200

    lab1

---
+++ lab1/script.sh
@@ -0,0 +1,24 @@
+echo '{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": "*",
+            "Resource":  "arn:aws:s3:::itstepfirstbucket/*",
+            "Condition": {
+                "NotIpAddress": {
+                    "aws:SourceIp": "50.31.252.0/24"
+                },
+                "Bool": {
+                    "aws:ViaAWSService": "false"
+                }
+            }
+        }
+    ]
+}' > policy.json
+
+aws s3api create-bucket --bucket itstepfirstbucket --region us-east-1 \
+&& aws s3api put-bucket-policy --bucket itstepfirstbucket --policy file://policy.json \
+&& aws s3 sync ./ s3://itstepfirstbucket/ \
+&& aws s3 website s3://itstepfirstbucket/ --index-document index.html --error-document error.html
