dd1fc54ffe7ed6eaced5fc2ade3b8e23a8f5e5e4
Vlada Mizerak <mizerak.vlada@gmail.com>
2022-12-03 19:55:47 +0200

    lab4-5

--- lab5/provision.sh
+++ lab5/provision.sh
@@ -1,6 +1,6 @@
 Topics=$(aws sns create-topic --name load_balancer_health --query 'TopicArn' --output text)
 Subscribe=$(aws sns subscribe --topic-arn $Topics --protocol email --notification-endpoint mizerak.vlada@gmail.com --query 'SubscriptionArn' --output text)
-aws cloudwatch put-metric-alarm --alarm-name Alarm --alarm-description "Instance in target group is deleted" --metric-name HealthyHostCount \
+aws cloudwatch put-metric-alarm --alarm-name Alarm --alarm-description "Instance in target group is deleted." --metric-name HealthyHostCount \
 --namespace AWS/ApplicationELB --statistic Average --period 60 --threshold 2 --comparison-operator LessThanThreshold \
 --dimensions Name=TargetGroup,Value=targetgroup/TargetGroup/6f9aca4fb77d2df3 Name=LoadBalancer,Value=app/LoadBalancer/2e713b709b250f51 \
 --evaluation-periods 1 --alarm-actions $Topics
